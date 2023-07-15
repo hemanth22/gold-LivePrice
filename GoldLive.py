ad391599280af157b00e8ffc33281e57eafdcb49
Hemanth B <hemanth22hemu@gmail.com>
2023-07-15 22:20:01 +0530

    XAU and XAG Currency update

--- GoldLive.py
+++ GoldLive.py
@@ -6,7 +6,7 @@ import os
 METAL_KEY = os.environ.get('METAL_KEY')
 METAL_HOST = os.environ.get('METAL_HOST')
 
-url = "https://live-metal-prices.p.rapidapi.com/v1/latest/XAU,XAG,PA,PL,GBP,EUR/INR/gram"
+url = "https://live-metal-prices.p.rapidapi.com/v1/latest/XAU,XAG/INR/gram"
 
 headers = {
 	"X-RapidAPI-Key": METAL_KEY,
@@ -15,4 +15,17 @@ headers = {
 
 response = requests.get(url, headers=headers)
 
-print(response.json())
+response = requests.get(url, headers=headers)
+
+loadJsonData = response.json()
+
+InitializationX = json.dumps(loadJsonData)
+DeinitilizationY = json.loads(InitializationX)
+
+buyCcy = DeinitilizationY['baseCurrency']
+buyUnit = DeinitilizationY['unit']
+GoldRate = DeinitilizationY['rates']['XAU']
+SilverRate = DeinitilizationY['rates']['XAG']
+
+print('1 {1} XAU = {2} {0}'.format(buyCcy, buyUnit, round(GoldRate,2)))
+print('1 {1} XAG = {2} {0}'.format(buyCcy, buyUnit, round(SilverRate,2)))
